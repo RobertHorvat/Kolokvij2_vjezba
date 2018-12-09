d5c71a0862eaa96679d82b2fd948cfe15def049a
Robert Horvat <robbie_horvat@hotmail.com>
2018-12-09 11:30:48 +0100

    Add project files.

---
+++ Kolokvij2_vjezba/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokvij2_vjezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
